42b86ad9c76138c8b9fd2f36503f596dba51e1b2
tadeufilho <tadeuvsfilho@gmail.com>
2024-08-25 23:12:42 -0300

    feat: melhoria de log da selecao do mcc da transacao

--- authorization/src/main/java/com/authorization/domain/model/MerchantCategoryCode.java
+++ authorization/src/main/java/com/authorization/domain/model/MerchantCategoryCode.java
@@ -3,6 +3,7 @@ package com.authorization.domain.model;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -10,6 +11,7 @@ import java.util.stream.Stream;
 import static com.authorization.core.util.ConstantsUtil.*;
 
 @Getter
+@Slf4j
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum MerchantCategoryCode {
 
@@ -26,6 +28,7 @@ public enum MerchantCategoryCode {
                         merchantCategoryCode.getCodes().stream()
                                 .anyMatch(c -> c.equalsIgnoreCase(code)))
                 .filter(merchantCategoryCode -> merchantCategoryCode.getName().equalsIgnoreCase(merchant))
+                .peek(mcc -> log.info("::: Merchant para requisicao -> {} :::", mcc) )
                 .findFirst()
                 .orElse(DEFAULT);
     }
